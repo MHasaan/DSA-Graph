ffd897f39b47ff0a0083dedfc0d7aeb42b52a793
MHasaan <131281292+MHasaan@users.noreply.github.com>
2023-12-21 07:22:54 +0000

    bfs search added

--- Graph.java
+++ Graph.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 class Graph
 {
     int elements;
@@ -8,17 +16,24 @@ class Graph
     public Graph()
     {
         size=5;
-        elements = 0;//how man vertices are present
-        adjMatrix = new int[5][5];  //the gaph itself
-        vertexList = new String[5]; //list of the vertices in graph
+        elements = 0; //how many vertices are present
+        adjMatrix = new int[size][size];  //the graph itself
+        vertexList = new Vertex[size]; //list of the vertices in graph
     }
 
-
-    int indexof(String label)
+    public Graph(int size)
     {
-        for(int i=0;i<this.size;i++)
+        this.size=size;
+        elements = 0; //how many vertices are present
+        adjMatrix = new int[size][size];  //the graph itself
+        vertexList = new Vertex[size]; //list of the vertices in graph
+    }
+
+    int indexOf(String label)
+    {
+        for(int i=0;i<this.elements;i++)
         {
-            if(vertexList[i].equals(label))
+            if(vertexList[i].getLabel().equals(label))
             {
                 return i;
             }
@@ -26,20 +41,17 @@ class Graph
         return -1;
     }
 
-
-
     public void addVertex(String label)
     {
         if (elements < size) 
         {
-            vertexList[elements++] = label;
-        } else 
+            vertexList[elements++] = new Vertex(label);
+        } 
+        else 
         {
             System.out.println("Graph full.");
         }
     }
-
-
 
     public void addEdge(String source, String destination) 
     {
@@ -53,26 +65,93 @@ class Graph
         } 
         else 
         {
-            System.out.println("vertices not found.");
+            System.out.println("Vertices not found.");
         }
     }
 
-    /////////////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////////////
 
 
+    // Finds a path between two nodes using Breadth-First Search.
+    public List<String> findPathBFS(String source, String destination) 
+    {
+        int sourceIndex = indexOf(source);
+        int destinationIndex = indexOf(destination);
 
-    // public boolean isNeighbour(Vertex first, Vertex second) {
+        if (sourceIndex == -1 || destinationIndex == -1)
+        {
+            System.out.println("Invalid source or destination vertex.");
+            return null;
+        }
+
+        boolean[] visited = new boolean[size];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(sourceIndex);
+        visited[sourceIndex] = true;
+
+        int[] parent = new int[size];
+        Arrays.fill(parent, -1);
+
+        while (!queue.isEmpty()) 
+        {
+            int currentVertex = queue.poll();//deque
+
+            if (currentVertex == destinationIndex) 
+            {
+                // Build and return the path
+                return buildPath(parent, sourceIndex, destinationIndex);
+            }
+
+            for (int neighbor = 0; neighbor < size; neighbor++) 
+            {
+                if (adjMatrix[currentVertex][neighbor] == 1 && !visited[neighbor]) 
+                {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                    parent[neighbor] = currentVertex;
+                }
+            }
+        }
+
+        System.out.println("Path not found between " + source + " and " + destination);
+        return null;
+    }
+
+    // Helper method to build the path from the parent array.
+    private List<String> buildPath(int[] parent, int sourceIndex, int destinationIndex) 
+    {
+        List<String> path = new ArrayList<>();
+        int currentVertex = destinationIndex;
+
+        while (currentVertex != -1) 
+        {
+            path.add(vertexList[currentVertex].getLabel());
+            currentVertex = parent[currentVertex];
+        }
+
+        // Reverse the path to get it from source to destination
+        Collections.reverse(path);
+        return path;
+    }
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+    // public boolean isNeighbour(Vertex first, Vertex second) 
+    // {
     //     int firstIndex = indexOf(first);
     //     int secondIndex = indexOf(second);
 
     //     return firstIndex != -1 && secondIndex != -1 && adjMatrix[firstIndex][secondIndex] == 1;
     // }
 
-    // public void bfs(Vertex start) {
+    
+    // public void BFS(Vertex start) 
+    // {
     //     int startIndex = indexOf(start);
 
-    //     if (startIndex == -1) {
+    //     if (startIndex == -1) 
+    //     {
     //         System.out.println("Start vertex not found.");
     //         return;
     //     }
@@ -83,35 +162,41 @@ class Graph
     //     visited[startIndex] = true;
     //     queue.add(start);
 
-    //     while (!queue.isEmpty()) {
+    //     while (!queue.isEmpty()) 
+    //     {
     //         Vertex current = queue.poll();
     //         System.out.println(current);
 
     //         int currentIndex = indexOf(current);
 
-    //         for (int i = 0; i < elements; i++) {
-    //             if (adjMatrix[currentIndex][i] == 1 && !visited[i]) {
+    //         for (int i = 0; i < elements; i++) 
+    //         {
+    //             if (adjMatrix[currentIndex][i] == 1 && !visited[i]) 
+    //             {
     //                 visited[i] = true;
     //                 queue.add(vertexList[i]);
     //             }
     //         }
     //     }
     // }
-
-    // public boolean isPath(Vertex from, Vertex to) {
+    // 
+    // public boolean isPath(Vertex from, Vertex to) 
+    // {
     //     int fromIndex = indexOf(from);
     //     int toIndex = indexOf(to);
 
     //     return fromIndex != -1 && toIndex != -1 && adjMatrix[fromIndex][toIndex] == 1;
     // }
 
-    // public int lengthOfPath(Vertex first, Vertex second) {
+    // public int lengthOfPath(Vertex first, Vertex second) 
+    // {
     //     // Implement using a graph traversal algorithm (e.g., BFS or Dijkstra's)
     //     // For simplicity, I'll use a modified BFS to find the shortest path
     //     int startIndex = indexOf(first);
     //     int endIndex = indexOf(second);
 
-    //     if (startIndex == -1 || endIndex == -1) {
+    //     if (startIndex == -1 || endIndex == -1) 
+    //     {
     //         System.out.println("Vertices not found.");
     //         return -1;
     //     }
@@ -123,16 +208,20 @@ class Graph
     //     visited[startIndex] = true;
     //     queue.add(startIndex);
 
-    //     while (!queue.isEmpty()) {
+    //     while (!queue.isEmpty()) 
+    //     {
     //         int currentIndex = queue.poll();
 
-    //         for (int i = 0; i < elements; i++) {
-    //             if (adjMatrix[currentIndex][i] == 1 && !visited[i]) {
+    //         for (int i = 0; i < elements; i++) 
+    //         {
+    //             if (adjMatrix[currentIndex][i] == 1 && !visited[i]) 
+    //             {
     //                 visited[i] = true;
     //                 distances[i] = distances[currentIndex] + 1;
     //                 queue.add(i);
 
-    //                 if (i == endIndex) {
+    //                 if (i == endIndex) 
+    //                 {
     //                     return distances[i];
     //                 }
     //             }
@@ -142,17 +231,21 @@ class Graph
     //     return -1; // No path found
     // }
 
-    // public int degreeOfVertex(Vertex vertex) {
+    // public int degreeOfVertex(Vertex vertex) 
+    // {
     //     int vertexIndex = indexOf(vertex);
 
-    //     if (vertexIndex == -1) {
+    //     if (vertexIndex == -1) 
+    //     {
     //         System.out.println("Vertex not found.");
     //         return -1;
     //     }
 
     //     int degree = 0;
-    //     for (int i = 0; i < elements; i++) {
-    //         if (adjMatrix[vertexIndex][i] == 1) {
+    //     for (int i = 0; i < elements; i++) 
+    //     {
+    //         if (adjMatrix[vertexIndex][i] == 1) 
+    //         {
     //             degree++;
     //         }
     //     }
@@ -160,11 +253,13 @@ class Graph
     //     return degree;
     // }
 
-    // public boolean isSource(Vertex vertex) {
+    // public boolean isSource(Vertex vertex) 
+    // {
     //     return degreeOfVertex(vertex) == 0;
     // }
 
-    // public boolean isSink(Vertex vertex) {
+    // public boolean isSink(Vertex vertex) 
+    // {
     //     return degreeOfVertex(vertex) == 0;
     // }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,39 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+import java.util.List;
 
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        // Create a graph
+        Graph graph = new Graph(6); // Adjust the size based on your needs
+
+        // Add vertices
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+
+        // Add edges
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "D");
+        graph.addEdge("C", "E");
+        graph.addEdge("D", "F");
+        graph.addEdge("E", "F");
+
+        // Test findPathBFS
+        String sourceVertex = "A";
+        String destinationVertex = "E";
+
+        System.out.println("Finding path from " + sourceVertex + " to " + destinationVertex);
+        List<String> path = graph.findPathBFS(sourceVertex, destinationVertex);
+
+        // Display the result
+        if (path != null) {
+            System.out.println("Path: " + path);
+            System.out.println("Path Length: " + (path.size() - 1)); // Length is the number of edges
+        }
     }
 }
